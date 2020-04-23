232ef231c7e558c004bfc2ac0aec7a771e7726a4
HDunlop <Dunlop.H@etoncollege.org.uk>
2020-04-23 12:38:42 +0800

    add pull steps to test2.py

--- test2.py
+++ test2.py
@@ -9,3 +9,6 @@ print("save first")
 print("open 'github' on the right hand side and 'stage' either the single file or all of the unstaged ones")
 print("add a commit message and commit to the selected branch")
 print("click 'push' beside whatever branch you are using")
+
+print("Pull:")
+print("right-click 'Fetch' and click 'Pull' dumbass")

fdd8721609af4fa22d16ec5b53e4d75711498b01
HDunlop <Dunlop.H@etoncollege.org.uk>
2020-04-23 12:35:23 +0800

    test3 -> test2

--- test2.py
+++ test2.py
@@ -1,4 +1,11 @@
+print("Create local respository:")
 print("Copy the link in the 'Clone or download' thing")
 print("Open Terminal or ur equivalent")
 print("write: git clone (copied link)")
 print("now u hav a local respository")
+
+print("Commit to git:")
+print("save first")
+print("open 'github' on the right hand side and 'stage' either the single file or all of the unstaged ones")
+print("add a commit message and commit to the selected branch")
+print("click 'push' beside whatever branch you are using")

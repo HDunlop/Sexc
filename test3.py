cee26e692dc65a65ecdd5da3370d5ce7a7593ce8
HDunlop <Dunlop.H@etoncollege.org.uk>
2020-04-23 12:35:44 +0800

    delete test3.py

--- test3.py
+++
@@ -1,4 +0,0 @@
-print("save first")
-print("open 'github' on the right hand side and 'stage' either the single file or all of the unstaged ones")
-print("add a commit message and commit to the selected branch")
-print("click 'push' beside whatever branch you are using")

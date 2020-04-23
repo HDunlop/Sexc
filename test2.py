4cb1e8b9f00d6c50181450da6869c004a7c839dc
HDunlop <Dunlop.H@etoncollege.org.uk>
2020-04-23 12:15:09 +0800

    Test commit

--- test2.py
+++ test2.py
@@ -1,1 +1,4 @@
-print("asalam alaykum")
+print("Copy the link in the 'Clone or download' thing")
+print("Open Terminal or ur equivalent")
+print("write: git clone (copied link)")
+print("now u hav a local respository")
---
+++ test3.py
@@ -0,0 +1,3 @@
+print("save first")
+print("open 'github' on the right hand side and 'stage' either the single file or all of the unstaged ones")
+print("add a commit message and commit to the selected branch")
